e4c386a2cda29ff0af7abe3bdc538986f444c0c0
Alina Ray <a.alina.ray@gmail.com>
2019-06-14 21:54:32 +0300

    Updates

---
+++ precache-manifest.10f141d74f88eaf3966ea6c49c26de05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84c03a34fd835a39fa3271874184acc",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "018ed52a3d25f58f0ee4",
+    "url": "/todo-app-react/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/todo-app-react/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "018ed52a3d25f58f0ee4",
+    "url": "/todo-app-react/static/js/main.6feeb518.chunk.js"
+  },
+  {
+    "revision": "8ed63381bb10a3952e6a",
+    "url": "/todo-app-react/static/js/runtime~main.ec7f1ccf.js"
+  }
+]);
